415d711232037de54afa6435c614f68b32dbb2dd
Guyutongxue <guyutongxue@163.com>
2020-04-18 21:05:09 +0800

    remove link

--- include/MahjongGB/MahjongGB.h
+++ include/MahjongGB/MahjongGB.h
@@ -1,1 +1,30 @@
-/mnt/d/Code/Cpp/Chinese-Standard-Mahjong/MahjongGB/MahjongGB.h
+#ifndef MAHJONG_H
+#define MAHJONG_H
+
+#include <utility>
+#include <vector>
+#include <string>
+
+//CPP
+// #include "MahjongGB.cpp"
+#include "../mahjong-algorithm/fan_calculator.h"
+#include "../mahjong-algorithm/shanten.h"
+//Dangerous
+
+using namespace std;
+
+void MahjongInit();
+
+vector<pair<int, string> > MahjongFanCalculator(
+    vector<pair<string, pair<string, int> > > pack,
+    vector<string> hand,
+    string winTile,
+    int flowerCount,
+    bool isZIMO,
+    bool isJUEZHANG,
+    bool isGANG,
+    bool isLAST,
+    int menFeng,
+    int quanFeng);
+
+#endif
